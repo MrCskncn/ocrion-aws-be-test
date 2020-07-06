a9b54db2ee8ff06e66cd85c16d1868c28727e3f2
Umur Coşkuncan <ghostonviper@gmail.com>
2020-07-06 14:28:00 +0300

    Update BasicController.java

--- src/main/java/com/ocrion/aws/test/controller/BasicController.java
+++ src/main/java/com/ocrion/aws/test/controller/BasicController.java
@@ -13,7 +13,7 @@ public class BasicController {
 
     @GetMapping("/greet")
     public ResponseEntity<?> greet(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name){
-        return ResponseEntity.ok(String.format("Hello %s! This is second attempt!",name));
+        return ResponseEntity.ok(String.format("Hello %s! This is third attempt!",name));
     }
 
     @PostMapping("/echo")
